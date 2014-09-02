ae11eb971d2fc81a32418f938fca6d8000a4d0a9
Eric B Merritt <eric@afiniate.com>
2014-09-02 09:31:34 +0200

    move copy-file-local from shell script to ocaml

--- lib/prj/prj.ml
+++ lib/prj/prj.ml
@@ -5,6 +5,7 @@ let name = "prj"
 
 let command =
   Command.group ~summary:"Project project tooling for the voteraise system"
-    [(Prj_build_remote.name, Prj_build_remote.command)]
+    [Prj_build_remote.desc;
+     Prj_copy_local.desc]
 
 let desc = (name, command)
---
+++ lib/prj/prj_copy_local.mli
@@ -0,0 +1,10 @@
+open Core.Std
+open Async.Std
+
+type error = No_afiniate_user
+exception Copy_error of error
+
+val name: String.t
+val command: Command.t
+
+val desc: String.t * Command.t
