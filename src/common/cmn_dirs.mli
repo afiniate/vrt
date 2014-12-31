0c85ff840e49f1063cb99b0983ed1c08aa5a9d5f
Eric B Merritt <eric@afiniate.com>
2014-12-31 11:52:19 -0800

    add a gather all dirs function to cmn_dirs

--- src/common/cmn_dirs.mli
+++ src/common/cmn_dirs.mli
@@ -5,7 +5,12 @@ open Async.Std
     directories *)
 
 val gather_dirs: String.t -> String.t List.t Deferred.t
-(** Given a root directory traverses the director gathering the names *)
+(** Given a root directory traverses the directory gathering the
+    names *)
+
+val gather_all_dirs: String.t List.t -> String.t List.t Deferred.t
+(** Given a list of root directory traverses the each directory
+    gathering the sub directories *)
 
 val change_to: String.t -> (Unit.t, Exn.t) Deferred.Result.t
 (** A shim to allow Unix.chdir to fit into a Result based Deferred *)
