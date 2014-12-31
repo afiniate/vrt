6c0aadfb5b0212f0d2b60ec1a400d5c2627df45e
Eric B Merritt <eric@afiniate.com>
2014-12-31 11:05:57 -0800

    remove references to voteraise from the help text

--- src/prj/prj.ml
+++ src/prj/prj.ml
@@ -4,7 +4,7 @@ open Async.Std
 let name = "prj"
 
 let command =
-  Command.group ~summary:"Project project tooling for the voteraise system"
+  Command.group ~summary:"Project project tooling for the ocaml systems"
     [Prj_build_remote.desc;
      Prj_copy_local.desc;
      Prj_mosh.desc;
