0f156a39a41ff3a0d70377a572029f0e1a011839
Eric B Merritt <eric@afiniate.com>
2014-12-31 16:11:55 -0800

    fix misspelling in make-dot-merlin command

--- src/prj/prj_make_dot_merlin.mli
+++ src/prj/prj_make_dot_merlin.mli
@@ -5,7 +5,7 @@ open Async.Std
  * This command creates a `.merlin` file in the project root when it is run
 *)
 
-exception Dot_merline_write_error
+exception Dot_merlin_write_error
 
 val name: String.t
 val command: Command.t
